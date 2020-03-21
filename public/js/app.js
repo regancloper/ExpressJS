981af63fb8e7e507c6a61548c71ba91db9cc5018
Regan Loper <reganloper@Regans-MBP.attlocal.net>
2020-03-21 11:20:57 -0500

    reformatted

--- public/js/app.js
+++ public/js/app.js
@@ -15,29 +15,29 @@ document.querySelector('[type="submit"]').addEventListener('click', (e) => {
     let email = document.querySelector('[type="email"]').value;
     fetch('/signup-form', {
         method: 'POST',
-        body: JSON.stringify({ name, email }), 
+        body: JSON.stringify({ name, email }),
         headers: {
             'Content-Type': 'application/json'
         }
     })
-    .then(res => {
-        if (res.ok) {
-            fetch('/formsubmissions')
-                .then(res => res.json())
-                .then(data => {
+        .then(res => {
+            if (res.ok) {
+                fetch('/formsubmissions')
+                    .then(res => res.json())
+                    .then(data => {
 
-                    const headers = document.querySelectorAll('h1');
-                    headers.forEach(header => document.body.removeChild(header));
-                    
+                        const headers = document.querySelectorAll('h1');
+                        headers.forEach(header => document.body.removeChild(header));
 
-                    data.forEach(user => {
-                        let h1 = document.createElement('h1');
-                        h1.textContent = user.name;
-                        document.body.append(h1);
-                    });
 
-                })
-        }
-    })
+                        data.forEach(user => {
+                            let h1 = document.createElement('h1');
+                            h1.textContent = user.name;
+                            document.body.append(h1);
+                        });
+
+                    })
+            }
+        })
 });
 
